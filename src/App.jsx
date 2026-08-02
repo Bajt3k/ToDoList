b99a11a94f3dbd8069560c50340c6f25055e0f00
Bartlomiej <bartox401@gmail.com>
2026-08-02 13:34:02 +0200

    Toggle Task Function

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import "./App.css";
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
 
     const trimmedTask = task.trim();
 
     if (trimmedTask !== "") {
-      setTasks((prevTasks) => [...prevTasks, trimmedTask]);
+      setTasks((prevTasks) => [
+        ...prevTasks,
+        { id: Date.now(), text: trimmedTask, done: false },
+      ]);
       setTask("");
     }
   }
+
+  function toggleTaskDone(id) {
+    setTasks((prevTasks) =>
+      prevTasks.map((t) => (t.id === id ? { ...t, done: !t.done } : t)),
+    );
+  }
+
   return (
     <>
       <h1>ToDoList</h1>
@@ -27,8 +38,15 @@ function App() {
         <button type="submit">ADD</button>
       </form>
       <ul>
-        {tasks.map((task, index) => (
-          <li key={`${task}-${index}`}>{task}</li>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}{" "}
+            <input
+              type="checkbox"
+              checked={task.done}
+              onChange={() => toggleTaskDone(task.id)}
+            />
+          </li>
         ))}
       </ul>
     </>
